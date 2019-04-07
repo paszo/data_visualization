281acadf3548823a3eaf8c304bff9d6e8278d3ff
popo <paszo2008@gmail.com>
2019-04-07 13:26:42 +0200

    Added function to get two letters codes from country names

---
+++ countries.py
@@ -0,0 +1,12 @@
+from pygal.maps.world import COUNTRIES
+
+
+def get_country_code(country_name):
+    """Return the Pygal 2-digit contry code for the given country."""
+    for code, name in COUNTRIES.items():
+        if name == country_name:
+            return code
+    # If the country wasn't found, return None.
+    return None
+
+
