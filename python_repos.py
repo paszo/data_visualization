d7ae70725c331a6708871254e4150ce4c9809fac
paszo <paszo2008@gmail.com>
2019-04-19 18:19:40 +0200

    Printing info about all repos

--- python_repos.py
+++ python_repos.py
@@ -13,9 +13,14 @@ print("Total repositories:", response_dict['total_count'])
 repo_dicts = response_dict['items']
 print("Repositories returned:", len(repo_dicts))
 
-# Examine the first repository.
-repo_dict = repo_dicts[0]
-print("\nKeys:", len(repo_dict))
-for key in sorted(repo_dict.keys()):
-    print(key)
+print("\nSelected information about each repository:")
 
+for repo_dict in repo_dicts:
+
+    print('Name', repo_dict['name'])
+    print('Owner', repo_dict['owner']['login'])
+    print('Stars', repo_dict['stargazers_count'])
+    print('Repository', repo_dict['html_url'])
+    print('Created', repo_dict['created_at'])
+    print('Updated', repo_dict['updated_at'])
+    print('Description', repo_dict['description'])
